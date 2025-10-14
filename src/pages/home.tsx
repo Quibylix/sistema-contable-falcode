c2c5d406ab83d4674e7367b3fd2546bea7338545
Quibylix <barraza.fredimanuel@gmai.com>
2025-10-14 10:30:24 -0600

    feat: modify home page to allow login when user is not logged

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,37 @@
+import { Container, Image, Text, Title } from "@mantine/core";
+import { useEffect } from "react";
+import { checkAuthTransaction } from "../features/auth/check-auth.transaction";
+import LoginForm from "../features/auth/login/login-form.component";
+
 export default function HomePage() {
+  useEffect(() => {
+    checkAuthTransaction()
+      .then(() => {
+        location.href = "/accounts";
+      })
+      .catch(() => {
+        console.log("No session found");
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container size="xs">
+      <Image
+        src="/falcode.jpeg"
+        radius="md"
+        alt="Logo Falcode"
+        w={150}
+        mb="md"
+        mt="xl"
+        mx="auto"
+      />
+      <Title order={1} ta="center" mt="md" mb="xl">
+        Sistema Contable para Falcode
+      </Title>
+      <LoginForm />
+      <Text mt={10} ta="center">
+        Usuario demo: <b>admin</b> / Contraseña: <b>1234</b>
+      </Text>
+    </Container>
   );
 }
