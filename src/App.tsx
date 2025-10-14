4dc1d333670a8b5adb8061e1e1e6276db8eb2a72
Quibylix <barraza.fredimanuel@gmai.com>
2025-10-13 21:18:45 -0600

    feat: configure mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/dates/styles.css";
+
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/home";
+import { MantineProvider } from "@mantine/core";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <MantineProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </MantineProvider>
   );
 }
