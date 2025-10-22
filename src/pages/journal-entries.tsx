e1a8d40ab09f61dfe5e3bec06ec6ac78ef16c229
Quibylix <barraza.fredimanuel@gmai.com>
2025-10-21 20:48:29 -0600

    feat: include register journal entry form with a modal

--- src/pages/journal-entries.tsx
+++ src/pages/journal-entries.tsx
@@ -1,9 +1,20 @@
-import { Container, Title } from "@mantine/core";
-import { useEffect } from "react";
+import { Button, Container, Title } from "@mantine/core";
+import { useEffect, useState } from "react";
 import { checkAuthTransaction } from "../features/auth/check-auth.transaction";
 import { RegisterJournalEntryForm } from "../features/journal-entries/register-journal-entry/register-journal-entry-form.component";
+import { Modal } from "@mantine/core";
 
 export default function JournalEntriesPage() {
+  const [modalOpened, setModalOpened] = useState(false);
+
+  function openModal() {
+    setModalOpened(true);
+  }
+
+  function closeModal() {
+    setModalOpened(false);
+  }
+
   useEffect(() => {
     checkAuthTransaction().catch(() => {
       location.href = "/";
@@ -14,15 +25,20 @@ export default function JournalEntriesPage() {
     <>
       <title>Registrar transacción | Sistema Contable Falcode</title>
       <Container size="lg">
+        <Button display="block" mx="auto" onClick={openModal}>
+          Registrar nueva transacción
+        </Button>
+        <Modal opened={modalOpened} onClose={closeModal} size="xl" mah="80vh">
+          <Container fluid>
+            <Title order={2} mb="md">
+              Registrar nueva transacción
+            </Title>
+            <RegisterJournalEntryForm />
+          </Container>
+        </Modal>
         <Title order={1} mt="md" mb="xl">
           Transacciones
         </Title>
-        <Container fluid>
-          <Title order={1} ta="center" mt="md" mb="xl">
-            Registrar Transacción
-          </Title>
-          <RegisterJournalEntryForm />
-        </Container>
       </Container>
     </>
   );
